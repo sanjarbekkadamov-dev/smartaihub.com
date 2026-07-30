ae5dee14eb268a9f71c0e41973a15723b6425ca8
sanjarbekkadamov-dev <sanjarbekkadamov@gmail.com>
2026-07-30 07:57:43 +0500

    Set up Express server with basic route

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ smartaihub
 ├── index.html
 ├── logo.png
 └── server.js
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("SmartAI Hub server ishlayapti");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server ishga tushdi");
+});
